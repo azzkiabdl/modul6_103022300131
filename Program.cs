ad07d443e6dfe51aeecdf15791ded17a3521a435
azzkiabdl <147375620+azzkiabdl@users.noreply.github.com>
2025-03-26 07:28:40 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

--- Program.cs
+++ Program.cs
@@ -1,2 +1,88 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random random = new Random();
+        this.id = id;
+        this.title = title;
+        this.playCount = playCount;
+    }
+
+    public void IncreasePlayCount(int playCount)
+    {
+        try
+        {
+            checked
+            {
+                this.playCount = Count;
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Error: Play count melebihi batas");
+        }
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"ID: {id}");
+        Console.WriteLine($"Title: {title}");
+        Console.WriteLine($"Play Count: {playCount}");
+    }
+
+    public int GetPlayCount()
+    {
+        return this.playCount;
+    }
+
+    public string GetTitle()
+    {
+        return this.title;
+    }
+}
+
+public class SayaTubeUser
+{
+    private int id;
+    private List<SayaTubeVideo> uploadedVideos;
+    public string Username;
+
+    public SayaTubeUser(string username)
+    {
+        Random random = new Random();
+        this.id = random.Next();
+        this.Username = username;
+        this.uploadedVideos = new List<SayaTubeVideo>();
+    }
+
+    public void AddVideo(SayaTubeVideo video)
+    {
+        uploadedVideos.Add(video);
+    }
+
+    public int GetTotalVideoPlayCount()
+    {
+        int total = 0;
+        foreach(var video in uploadedVideos)
+        {
+            total = video.GetPlayCount();
+        }
+        return total;
+    }
+
+    public void PrintAllVideoPlaycount()
+    {
+        Console.WriteLine($"User: {Username}");
+        for (int i = 0; i < Math.Min(uploadedVideos.Count, 8); i++)
+        {
+            Console.WriteLine($"Video {i + 1} judul: {uploadedVideos[i].GetTitle()}");
+        }
+    }
+}
